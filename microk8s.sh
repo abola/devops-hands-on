79727c2980b30fff14368b5f784ac59eb5ad0cce
Abola Lee <abola921@gmail.com>
2020-06-15 11:31:36 +0800

    增加 HPA metrics support

--- microk8s.sh
+++ microk8s.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+###
+# MicroK8s + Rancher installer
+##
+
 # 輸入您的 email 
 EMAIL=
 
@@ -5,7 +10,7 @@ EMAIL=
 EXTERNAL_IP=$(curl -s 169.254.169.254/computeMetadata/v1beta1/instance/network-interfaces/0/access-configs/0/external-ip)
 
 sudo snap install microk8s --channel=1.18 --classic
-sudo microk8s.enable dns dashboard storage ingress helm3 rbac
+sudo microk8s.enable dns dashboard storage ingress helm3 rbac 
 
 sudo sh -c 'echo "--allow-privileged=true" >> /var/snap/microk8s/current/args/kube-apiserver'
 sudo systemctl restart snap.microk8s.daemon-apiserver.service
@@ -15,23 +20,27 @@ sudo microk8s.kubectl create namespace cert-manager
 sudo microk8s.kubectl label namespace cert-manager certmanager.k8s.io/disable-validation=true
 sudo microk8s.helm3 install cert-manager jetstack/cert-manager --namespace cert-manager --version v0.15.1 --set installCRDs=true
 
-while [ `sudo microk8s.kubectl get po -n cert-manager | grep '1/1' | wc -l` -lt 3 ]
-do
-  sleep 5
-  echo "等待 cert-manager 服務啟動 ..."
-done
+echo "等待 cert-manager 服務啟動 ..."
+sleep 3
+sudo microk8s.kubectl wait --for=condition=ready --timeout=120s pods -l app=cert-manager -n cert-manager
+sudo microk8s.kubectl wait --for=condition=ready --timeout=120s pods -l app=cainjector -n cert-manager
+sudo microk8s.kubectl wait --for=condition=ready --timeout=120s pods -l app=webhook -n cert-manager
+sleep 3
 
 sudo microk8s.helm3 repo add rancher-stable https://releases.rancher.com/server-charts/stable
 sudo microk8s.kubectl create namespace cattle-system
 sudo microk8s.helm3 install rancher rancher-stable/rancher  --namespace cattle-system --set replicas=1 --set hostname=rancher.${EXTERNAL_IP}.nip.io --set ingress.tls.source=letsEncrypt --set letsEncrypt.email=${EMAIL}
 
-while [ `sudo microk8s.kubectl get po -n cattle-system | grep ^rancher | grep '1/1' | wc -l` -eq 0 ]
-do
-  sleep 10
-  echo "Rancher 啟動中請稍候 ... "
-done
+echo "等待 Rancher 服務啟動 ..."
+sleep 3
+sudo microk8s.kubectl wait --for=condition=ready --timeout=120s pods -l app=rancher -n cattle-system
+sleep 3
 
 sudo microk8s.kubectl patch $(sudo microk8s.kubectl get user.management.cattle.io -l authz.management.cattle.io/bootstrapping=admin-user -o name) --type='json' -p '[{"op":"replace","path":"/mustChangePassword","value":false},{"op":"replace","path":"/password","value":"$2a$10$7BhfjPOlS.KyLj81XMLWkO/ZH7JqeB1xeBmJzygZCHbD7Xni9Exy2"}]'
+
+sudo microk8s.enable metrics-server
+sudo git clone https://github.com/abola/2day_calalogs.git
+sudo microk8s.helm3 install flagger -n default --set=externalIp=${EXTERNAL_IP} ./2day_catalogs/charts/flagger-server/0.27.0/
 
 echo "----------------------------------------"
 echo "安裝完成"
